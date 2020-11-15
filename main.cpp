42309dda854a05014780a86ed457650217e05b5a
Skyler Young <SkylerYoung@mac.com>
2020-11-15 06:53:23 +0000

    Answered 1-8

--- main.cpp
+++ main.cpp
@@ -32,28 +32,286 @@ Purpose:  This project continues developing Project3.
  
  */
 
+#include <iostream>
 /*
  copied UDT 1:
  */
+struct Shoe
+{
+    std::string brand { "Nike" };
+    double size { 0 };
+    std::string color { "white" };
+    std::string condition { "good" };
+    std::string securingMethod { "velcro" };
+    double percentOfSoleLeft = 100;
+    bool beingWorn = false;
+    
+    Shoe();
+    ~Shoe()
+    {
+        std::cout << "Shoe Destroyed\n";
+    }
 
+    void putOn ( std::string firstShoe = "R", bool leftFirst = true);
+    void takeOff ( std::string firstShoe = "R", bool leftFirst = true );
+    void throwAway ( bool donate = false );
+    void wearDown(int timesWorn);
+};
+
+Shoe::Shoe(){}
+
+void Shoe::putOn(std::string firstShoe, bool leftFirst) 
+{
+    if (leftFirst)
+    {
+        firstShoe = "L";
+    }
+}
+
+void Shoe::takeOff(std::string firstShoe, bool leftFirst) 
+{
+    if (leftFirst)
+    {
+        firstShoe = "L";
+    }
+}
+
+void Shoe::throwAway(bool donate) 
+{
+    if (condition == "good")
+    {
+        donate = true;
+    }
+    
+}
+
+void Shoe::wearDown(int timesWorn)
+{
+    for (int i = 0; i <= timesWorn; i += 1)
+    {
+        percentOfSoleLeft -= 0.2;
+    }
+
+    std::cout << "Sole percent left " << percentOfSoleLeft << std::endl;
+}
 /*
  copied UDT 2:
  */
+struct Wurlitzer
+{
+    int volumeLevel;
+    double vibratoAmount;
+    std::string model;
+    std::string condition;
+    int numKeys;
+    int output = 23;
+    bool on;
+    int tuning = 440;
 
+    Wurlitzer();
+    ~Wurlitzer()
+    {
+        std::cout << "Wurlitzer Destroyed\n";
+    }
+
+    void changeVolume ( int knobPosition );
+    void play ( int note = 23 );
+    void power ( bool switchPosition = true );
+    void printCondition();
+    int swellVolume(int amount, int time);
+};
+
+Wurlitzer::Wurlitzer() :
+volumeLevel ( 6 ),
+vibratoAmount ( .5 ),
+model ( "200a" ),
+condition ( "good" ),
+numKeys ( 49 ),
+output ( 23 ),
+on ( false )
+{
+
+}
+
+void Wurlitzer::changeVolume( int knobPosition ) 
+{
+    volumeLevel = knobPosition;
+    std::cout << "Knob Position is: " << knobPosition << "\n";
+}
+
+void Wurlitzer::play( int note ) 
+{
+    output = note;
+}
+
+void Wurlitzer::power( bool switchPosition ) 
+{
+    on = switchPosition;
+}
+
+void Wurlitzer::printCondition()
+{
+    std::cout << "This Wutlizer is in " << condition << " condition\n"; 
+}
+
+int Wurlitzer::swellVolume(int amount, int time)
+{
+    int swell = 0;
+    for (int i = 0; i <= time ; i += 1)
+    {
+        swell += 20;
+        if (swell >= amount)
+        {
+            std::cout << "Swell finished" << std::endl;
+            return swell;
+        }
+    }
+    return swell;
+}
 /*
  copied UDT 3:
  */
+struct ElectricBass
+{
+    int numStrings = 4;
+    std::string pickup = "precision";
+    std::string brand = "fender";
+    std::string model = "precision";
+    int yearMade = 1974;
+    int output = 23;
+    int cost = 1544;
+    int timeSinceRestrung = 13;
+    bool timeToRestring = false;
+    int tuning = 442;
+    
+    ElectricBass();
+    ~ElectricBass()
+    {
+        std::cout << "Wurlitzer Destroyed\n";
+    }
 
+    void play ( int note =  18 );
+    void sold ( int price = 1200 );
+    void restring ( bool restring = true );
+    void checkStrings (int timesPlayed);
+};
+
+ElectricBass::ElectricBass(){}
+
+void ElectricBass::play( int note ) 
+{
+    output = note;
+}
+
+void ElectricBass::sold( int price ) 
+{
+    cost = price;
+    std::cout << "This Bass was sold for $" << price << "\n";
+}
+
+void ElectricBass::restring( bool restring ) 
+{
+    if (restring)
+    {
+        timeSinceRestrung = 0;
+    }
+}
+
+void ElectricBass::checkStrings (int timesPlayed)
+{
+    for (int i = 0; i <= timesPlayed; i += 1)
+    {
+        timeSinceRestrung += 1; 
+    }
+
+    if (timeSinceRestrung > 60)
+    {
+        timeToRestring = true;
+        std::cout << "Time to change strings!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Strings are still good!" << std::endl;
+    }
+} 
 /*
  new UDT 4:
  with 2 member functions
  */
+struct band
+{
+    Wurlitzer keys;
+    ElectricBass bass;
+
+    void tune()
+    {
+        keys.tuning = bass.tuning;
+    }
+
+    void turnDown(int amount)
+    {
+        keys.output -= amount;
+        bass.output -= amount;
+    }
+
+    band();
+    ~band();
+};
+
+band::band(){}
+band::~band()
+{
+        if (keys.on)
+        {
+            std::cout << "Turn off Wurlitzer\n";
+        }
+}
 
 /*
  new UDT 5:
  with 2 member functions
  */
+struct clothing
+{
+    Shoe blazer;
+    Shoe vans;
+    bool vansBeingWorn = false;
+    void switchShoes()
+    {
+        if (blazer.beingWorn == true)
+        {
+            blazer.beingWorn = false;
+            vans.beingWorn = true;
+        }
+        else
+        {
+            blazer.beingWorn = true;
+        }
+    }
 
+    void changeSize(int amount)
+    {
+        blazer.size += amount;
+        vans.size += amount;
+    }
+
+    clothing();
+    ~clothing();
+
+};
+
+clothing::clothing(){}
+clothing::~clothing()
+{
+        if (vansBeingWorn)
+        {
+            std::cout << "Shoe being worn is vans\n";
+        }
+        else
+        {
+            std::cout << "Shoe being worn is blazer\n";
+        }
+    }
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
@@ -71,5 +329,25 @@ Purpose:  This project continues developing Project3.
 #include <iostream>
 int main()
 {
-    std::cout << "good to go!" << std::endl;
+    Shoe blazer, airforce1;
+    Wurlitzer red, tan;
+    ElectricBass jaguar, precision;
+    band bar, concert;
+    clothing chris;
+
+    jaguar.play(24);
+    tan.power(true);
+    blazer.putOn("L", false);
+
+    tan.printCondition();
+
+    blazer.wearDown(47);
+
+    precision.checkStrings(84);
+
+    red.output = red.swellVolume(60, 5);
+
+    bar.turnDown(6);
+
+    chris.changeSize(1);
 }
