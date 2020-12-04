9c29d287199391a65d836674977a2b211e0f1599
Skyler Young <SkylerYoung@mac.com>
2020-12-04 03:10:50 +0000

    Revision 3

--- Wrappers.cpp
+++ Wrappers.cpp
@@ -1,16 +1,16 @@
 #include "Wrappers.h"
 
-ShoeWrapper::ShoeWrapper(Shoe* shoePtr) : theShoe(shoePtr) { }
+ShoeWrapper::ShoeWrapper( Shoe* shoePtr ) : theShoe(shoePtr) { }
 ShoeWrapper::~ShoeWrapper() { delete theShoe; }
 
-WurlitzerWrapper::WurlitzerWrapper(Wurlitzer* wurlitzerPtr) : theWurli(wurlitzerPtr) { }  
+WurlitzerWrapper::WurlitzerWrapper( Wurlitzer* wurlitzerPtr ) : theWurli(wurlitzerPtr) { }  
 WurlitzerWrapper::~WurlitzerWrapper() { delete theWurli; }
 
-BassWrapper::BassWrapper(ElectricBass* bassPtr) : theBass(bassPtr) { }
+BassWrapper::BassWrapper( ElectricBass* bassPtr ) : theBass(bassPtr) { }
 BassWrapper::~BassWrapper() { delete theBass; }
 
-BandWrapper::BandWrapper(Band* bandPtr) : theBand(bandPtr) { }
+BandWrapper::BandWrapper( Band* bandPtr ) : theBand(bandPtr) { }
 BandWrapper::~BandWrapper() { delete theBand; }
 
-ClothingWrapper::ClothingWrapper(Clothing* clothesPtr) : theClothes(clothesPtr) { }
+ClothingWrapper::ClothingWrapper( Clothing* clothesPtr ) : theClothes(clothesPtr) { }
 ClothingWrapper::~ClothingWrapper() { delete theClothes; }
